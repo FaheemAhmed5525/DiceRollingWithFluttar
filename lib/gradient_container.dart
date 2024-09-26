03739df8eb5c1e54e1c418001b7efca51ca0885c
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-26 15:37:18 +0500

    Layout created

---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  
+final Color color1;
+final Color color2;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [color1, color2],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+      )),
+      child: const Center(
+        child: Text("Image"),
+      ),
+    );
+  }
+}
+
+
+
+
